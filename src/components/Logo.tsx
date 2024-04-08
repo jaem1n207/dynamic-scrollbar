0c562b531227f00d8c682e8457336791e2519ad4
jaem1n207 <roy.jm.lee@gmail.com>
2024-04-08 15:03:21 +0900

    chore: 잡다한 수정

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,15 @@
-import IconPower from '~icons/pixelarticons/power';
+import IconLightningStandRounded from '~icons/material-symbols/lightning-stand-rounded';
 
 export default function Logo() {
   return (
     <a
       className="icon-btn mx-2 text-2xl"
       rel="noreferrer"
-      href="https://github.com/antfu/vitesse-webext"
+      href="https://github.com/jaem1n207/dynamic-scrollbar"
       target="_blank"
       title="GitHub"
     >
-      <IconPower />
+      <IconLightningStandRounded />
     </a>
   );
 }

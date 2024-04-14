c4f36c3bc939c021cc8910e99b4f34be7f0a1887
이재민 <jaem1n207@naver.com>
2024-04-15 01:00:23 +0900

    feat: unocss with shadcn

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'unocss/vite';
 import { presetAttributify, presetIcons, presetUno, transformerDirectives } from 'unocss';
+import presetAnimations from 'unocss-preset-animations';
+import { builtinColors, presetShadcn } from 'unocss-preset-shadcn';
 
 export default defineConfig({
-  presets: [presetUno(), presetAttributify(), presetIcons()],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+    presetAnimations(),
+    presetShadcn(builtinColors.map((c) => ({ color: c }))),
+  ],
   transformers: [transformerDirectives()],
 });

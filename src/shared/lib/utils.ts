0d0cf193414d6c8d8a921e9eacfcdfc30d9db8cd
jaem1n207 <roy.jm.lee@gmail.com>
2024-04-24 18:29:50 +0900

    feat: 읽기 전용 배열 생성하는 유틸 함수 구현

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -5,3 +5,25 @@ import { twMerge } from 'tailwind-merge';
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+const arrayCache: Record<number, readonly number[]> = {};
+/**
+ * Create shared fixed-size read-only arrays.
+ * @example
+ * ```jsx
+ * <div className="space-y-2">
+ *  {createFixedArray(5).map((i) => (
+ *     <Skeleton key={i} className="h-4 w-[200px]" />
+ *   ))}
+ * </div>
+ * ```
+ */
+export const createFixedArray = (length: number): readonly number[] => {
+  arrayCache[length] ||=
+    arrayCache[length] ||
+    Array(length)
+      .fill(0)
+      .map((_, i) => i);
+
+  return arrayCache[length];
+};

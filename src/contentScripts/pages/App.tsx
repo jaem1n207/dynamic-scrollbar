e55e0eddbfbd440c3abf19108cc5d747c37719dc
jaem1n207 <tech.jmtt@gmail.com>
2024-05-20 02:42:16 +0900

    feat: 브라우저가 전체 화면 모드로 전환된다면 콘텐츠 숨김 처리

--- src/contentScripts/pages/App.tsx
+++ src/contentScripts/pages/App.tsx
@@ -3,7 +3,35 @@ import 'uno.css';
 
 import { Button } from '~/shared/ui';
 
+const subscribeSystemFullscreen = (onStoreChange: () => void) => {
+  document.addEventListener('fullscreenchange', onStoreChange);
+
+  return () => document.removeEventListener('fullscreenchange', onStoreChange);
+};
+
+const getSystemFullscreenSnapshot = () => {
+  return document.fullscreenElement !== null;
+};
+
+const getServerSnapshot = () => {
+  return undefined;
+};
+
+const useSystemFullscreen = () => {
+  return useSyncExternalStore(
+    subscribeSystemFullscreen,
+    getSystemFullscreenSnapshot,
+    getServerSnapshot,
+  );
+};
+
 export const App = () => {
+  const isFullScreen = useSystemFullscreen();
+
+  if (isFullScreen) {
+    return null;
+  }
+
   return (
     <div className="fixed h-dvh w-0 top-0 right-0 theme-blue">
       <div className="absolute w-14 select-none top-0 right-0 transform-translate-x-0 transform-translate-y-80">
